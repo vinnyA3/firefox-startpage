80b38721c09978dc2ba3e9d79add051f0bb8f46d
vinnyA3 <vincent.aceto@gmail.com>
2018-02-20 09:58:01 -0500

    feat(app): add helpful fn signatures + comment out IO

--- src/app.js
+++ src/app.js
@@ -1,23 +1,27 @@
 import 'main.scss'
-
-import { IO } from 'monet'
+// import { IO } from 'monet'
 import { take } from 'ramda'
 import Future from 'fluture'
 
+// futureFetch :: Future Promise
 const futureFetch = Future.encaseP(fetch)
 
-const safeToJSON = data => Future.tryP(() => data.json()) 
+// safeParseJSON :: Object -> Future Rejected JSON
+const safeParseJSON = data => Future.tryP(() => data.json()) 
 
+// getStoryData :: Array -> Future Array
 const getStoryData = ids =>
     Future.parallel(5, ids.map(id =>
         futureFetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
-            .chain(safeToJSON)))
+            .chain(safeParseJSON)))
 
+// getTopStoriesIds :: Future
 const getTopStoriesIds =
     futureFetch('https://hacker-news.firebaseio.com/v0/topstories.json') 
-        .chain(safeToJSON)
+        .chain(safeParseJSON)
         .map(take(10))
 
+// calling all stories ...
 getTopStoriesIds
     .chain(getStoryData)
     .fork(console.error, console.log)
