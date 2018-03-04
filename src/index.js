a81f9b8a4e96b5bc3136ed079cc319bd0b249b7e
vinnyA3 <vincent.aceto@gmail.com>
2018-03-04 11:42:57 -0500

    feat(index): split concerns, import component

--- src/index.js
+++ src/index.js
@@ -1,58 +1,10 @@
-import xs from 'xstream'
-import { run } from '@cycle/xstream-run'
-import { timeDriver } from '@cycle/time'
 import { makeDOMDriver } from '@cycle/dom'
 import { makeHTTPDriver } from '@cycle/http'
-import { html } from 'snabbdom-jsx'
+import { run } from '@cycle/most-run'
 
-const githubInfoTemplate = ({ name, blog, location, public_repos }) =>
-  <div>
-    <h2>{ name }</h2>
-    <h3>{ blog }</h3>
-    <h3>{ location }</h3>
-    <h3>Public Repos: { public_repos }</h3>
-  </div>
+import GithubSearch from 'components/GithubSearch'
 
-function main ({ DOM, HTTP, TIME }) {
-  const category = 'user'
-
-  const input$ = DOM.select('input').events('input')
-    .map(e => e.target.value)
-    .startWith('')
-
-  const fetchGihubUser$ = input$
-    .filter(val => val !== '')
-    .compose(TIME.debounce(600))
-    .map(user => ({
-      method: 'GET',
-      url: `https://api.github.com/users/${user}`,
-      category
-    }))
-
-  const getResult$ = HTTP.select(category)
-    .flatten()
-    .map(res => res.body)
-    .startWith(null)
-
-  return {
-    DOM: view(getResult$),
-    HTTP: fetchGihubUser$
-  }
-}
-
-function view (input$) {
-  return input$.map(user =>
-    <div>
-      <input type='text' />
-      <ul>
-        { user !== null ? githubInfoTemplate(user) : '' }
-      </ul>
-    </div>
-  )
-}
-
-run(main, {
+run(GithubSearch, {
   DOM: makeDOMDriver('#app'),
-  HTTP: makeHTTPDriver(),
-  TIME: timeDriver
+  HTTP: makeHTTPDriver()
 })
